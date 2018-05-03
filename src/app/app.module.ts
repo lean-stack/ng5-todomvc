de7aea4c7e830472356d0b0d82753f3efdb80ce9
Micha Alt <mail@lean-stack.de>
2018-05-03 08:52:38 +0200

    app: todos module imported.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TodosModule } from './todos/todos.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
