719c0f7ad0f49206cec7fd38d5ae7e00bb7045e8
Micha Alt <mail@lean-stack.de>
2018-05-03 08:53:52 +0200

    ng: todos components created.

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [RootComponent, HeaderComponent, MainComponent, FooterComponent, ListComponent, ItemComponent]
 })
 export class TodosModule { }
